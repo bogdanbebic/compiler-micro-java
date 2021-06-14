30e6456ff45d0a284e6b5ca36da7da4ac6a698b0
Bogdan Bebić <36513588+bogdanbebic@users.noreply.github.com>
2021-06-14 19:09:16 +0200

    Add global symbols chaining for symbol table

--- src/rs/ac/bg/etf/pp1/SemanticAnalyzer.java
+++ src/rs/ac/bg/etf/pp1/SemanticAnalyzer.java
@@ -1,7 +1,25 @@
 package rs.ac.bg.etf.pp1;
 
+import rs.ac.bg.etf.pp1.ast.Program;
+import rs.ac.bg.etf.pp1.ast.ProgramHeader;
 import rs.ac.bg.etf.pp1.ast.VisitorAdaptor;
+import rs.etf.pp1.symboltable.concepts.Obj;
 
 public class SemanticAnalyzer extends VisitorAdaptor {
     // TODO: implement
+
+    @Override
+    public void visit(ProgramHeader programHeader) {
+        super.visit(programHeader);
+        programHeader.obj = MJSymbolTable.insert(Obj.Prog, programHeader.getProgramName(), MJSymbolTable.noType);
+        MJSymbolTable.openScope();
+    }
+
+    @Override
+    public void visit(Program program) {
+        super.visit(program);
+        MJSymbolTable.chainLocalSymbols(program.getProgramHeader().obj);
+        MJSymbolTable.closeScope();
+    }
+
 }
