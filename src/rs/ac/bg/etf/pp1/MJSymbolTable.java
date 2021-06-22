2d2b83a59363520576060da638ef9c91dc9100cd
Bogdan Bebić <36513588+bogdanbebic@users.noreply.github.com>
2021-06-22 13:39:48 +0200

    SemanticAnalyzer, MJSymbolTable: Add print and read semantics checking

--- src/rs/ac/bg/etf/pp1/MJSymbolTable.java
+++ src/rs/ac/bg/etf/pp1/MJSymbolTable.java
@@ -1,5 +1,7 @@
 package rs.ac.bg.etf.pp1;
 
+import rs.ac.bg.etf.pp1.ast.Designator;
+import rs.ac.bg.etf.pp1.ast.DesignatorArrayIndex;
 import rs.etf.pp1.symboltable.Tab;
 import rs.etf.pp1.symboltable.concepts.Obj;
 import rs.etf.pp1.symboltable.concepts.Struct;
@@ -13,4 +15,16 @@ public class MJSymbolTable extends Tab {
         Tab.currentScope.addToLocals(new Obj(Obj.Type, "bool", boolType));
     }
 
+    public static boolean isNotBuiltinType(Struct struct) {
+        return !intType.equals(struct) && !charType.equals(struct) && !boolType.equals(struct);
+    }
+
+    public static boolean isAssignable(Designator designator) {
+        if (designator.obj.getType().getKind() == Struct.Array &&
+                designator instanceof DesignatorArrayIndex) {
+            return isAssignable(((DesignatorArrayIndex) designator).getDesignator());
+        }
+
+        return designator.obj.getKind() == Obj.Var;
+    }
 }
